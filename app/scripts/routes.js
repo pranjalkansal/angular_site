b409ed5fca278057b4bdabc1207395ada62a2c45
pranjal <pranjalkansal@gmail.com>
2016-04-10 14:42:05 +0530

    create empty routes file

---
+++ app/scripts/routes.js
@@ -0,0 +1,10 @@
+/******************************************************
+                                                      *
+  Module: routes.js                                   *
+  Functionality: To resole routes for defined states  *
+                                                      *
+******************************************************/
+
+'use strict';
+
+Angular.config();
--- app/scripts/services/server_communication.js
+++ app/scripts/services/server_communication.js
@@ -2,6 +2,17 @@
 
 'use strict';
 
-const URL = 'http://127.0.0.1:8000';
+const URL = 'http://127.0.0.1:8000'; // Server url.
 
-Angular.service('Server', function () {});
+Angular.service('Server', function () {
+  var database = {};
+
+  // Create fetch object for GET calls from server.
+  database.fetch = {};
+
+  // Create write object for POST calls to server.
+  database.write = {};
+
+  // Create update object for PUT or DELETE calls to server.
+  database.update = {};
+});
