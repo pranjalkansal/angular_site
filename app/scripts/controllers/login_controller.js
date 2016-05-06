9d6886c5e380ad63ab4176a1069f10e616a3f17a
Pranjal <pranjalkansal@gmail.com>
2016-05-06 17:27:41 +0530

    constant regex added

--- app/scripts/controllers/login_controller.js
+++ app/scripts/controllers/login_controller.js
@@ -7,16 +7,16 @@
 
 'use strict';
 
-Angular.controller('login', ['$scope', '$state', '$http', function ($scope, $state, $http) {
+Angular.controller('login', ['$scope', '$state', '$http', 'REGEX', function ($scope, $state, $http, REGEX) {
 
   // Initialize empty object to collect data from form
   $scope.login = {};
 
   // Create regex for validations.
   $scope.login.validation = {};
-  $scope.login.validation.email_pattern = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i; //parsley regex
-  $scope.login.validation.name_pattern = /^[a-z ,.'-]+$/i;
-  $scope.login.validation.username_pattern = /^\w+$/;
+  $scope.login.validation.email_pattern = REGEX.email;
+  $scope.login.validation.name_pattern = REGEX.name;
+  $scope.login.validation.username_pattern = REGEX.alpha_num;
 
   // Check for state and activate the login/register tabs and contents.
   $scope.login.active = $state.current.url == '/login'?true:false;
